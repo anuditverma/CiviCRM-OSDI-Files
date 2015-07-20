c312caa4907e1b7b8de265b07bf47a5613a99781
Anudit Verma <verma.anudit@gmail.com>
2015-07-20 18:45:37 +0530

    Pushed osdi.php
    This file includes AEP for accessing other resources available on the server through HAL-browser.

---
+++ sites/all/modules/civicrm/extern/osdi.php
@@ -0,0 +1,20 @@
+{
+    "motd": "Welcome to CiviCRM AEP!",
+    "_links": {
+        "curies": [
+            {
+                "name": "osdi",
+                "href": "https://osdi-sample-system.org/docs/v1/{rel}",
+                "templated": true
+            }
+        ],
+        "self": {
+            "href": "http://camus.fuzion.co.nz/sites/all/modules/civicrm/api",
+            "title": "This API entry point"
+        },
+        "osdi:people": {
+            "href": "http://camus.fuzion.co.nz/hal-browser/browser.html#/sites/all/modules/civicrm/extern/osdi.php?entity=People&action=get",
+            "title": "The collection of people in the system"
+        }        
+    }
+}
