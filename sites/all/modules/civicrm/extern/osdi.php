eab531600d7e0f05d324a083e19cc34cf0a2dea2
Anudit Verma <verma.anudit@gmail.com>
2015-07-23 05:27:26 +0530

    Modified osdi.php
    Now AEP is in php coding and returns JSON representation.

--- sites/all/modules/civicrm/extern/osdi.php
+++ sites/all/modules/civicrm/extern/osdi.php
@@ -1,20 +1,13 @@
-{
-    "motd": "Welcome to CiviCRM AEP!",
-    "_links": {
-        "curies": [
-            {
-                "name": "osdi",
-                "href": "https://osdi-sample-system.org/docs/v1/{rel}",
-                "templated": true
-            }
-        ],
-        "self": {
-            "href": "http://camus.fuzion.co.nz/sites/all/modules/civicrm/api",
-            "title": "This API entry point"
-        },
-        "osdi:people": {
-            "href": "http://camus.fuzion.co.nz/hal-browser/browser.html#/sites/all/modules/civicrm/extern/osdi.php?entity=People&action=get",
-            "title": "The collection of people in the system"
-        }        
-    }
-}
+<?php
+
+$params['motd'] = 'Welcome to CiviCRM AEP!';
+$params['_links'] = array(
+    'curies' => array(
+        array('name' => 'osdi','href' => 'https://osdi-sample-system.org/docs/v1/{rel}','templated' => true),
+       ),
+    'self' => array('href' => '/sites/all/modules/civicrm/extern/osdi.php','title' => 'This API entry point'),
+    'osdi:people' => array('href' => '/sites/all/modules/civicrm/extern/osdi.php?entity=People&action=get',
+                            'title' => 'The collection of people in the system')
+);
+
+echo json_encode($params);
