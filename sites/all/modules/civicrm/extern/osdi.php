280509499fffb59b758433b6bc4d65838eef60be
Anudit Verma <verma.anudit@gmail.com>
2015-07-24 00:12:00 +0530

    Update people's resource link
    Link now includes rest.php along with API & site keys.

--- sites/all/modules/civicrm/extern/osdi.php
+++ sites/all/modules/civicrm/extern/osdi.php
@@ -6,7 +6,7 @@ $params['_links'] = array(
         array('name' => 'osdi','href' => 'https://osdi-sample-system.org/docs/v1/{rel}','templated' => true),
        ),
     'self' => array('href' => '/sites/all/modules/civicrm/extern/osdi.php','title' => 'This API entry point'),
-    'osdi:people' => array('href' => '/sites/all/modules/civicrm/extern/osdi.php?entity=People&action=get',
+    'osdi:people' => array('href' => '/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={%22sequential%22:1,%22return%22:%22magicword%22,%22magicword%22:%22sesame%22}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ',
                             'title' => 'The collection of people in the system')
 );
 
