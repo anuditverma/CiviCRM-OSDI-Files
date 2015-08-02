0c984bdda52c84efcb4b9ee805b96b94b3e016b8
Anudit Verma <verma.anudit@gmail.com>
2015-08-03 00:50:51 +0530

    Updated osdi.php
    Added more links for various resource

--- sites/all/modules/civicrm/extern/osdi.php
+++ sites/all/modules/civicrm/extern/osdi.php
@@ -3,10 +3,13 @@
 $params['motd'] = 'Welcome to CiviCRM AEP!';
 $params['_links'] = array(
     'curies' => array(
-        array('name' => 'osdi','href' => 'https://osdi-sample-system.org/docs/v1/{rel}','templated' => true),
+        array('name' => 'osdi','href' => '/sites/default/ext/osdi/api/v3/{rel}','templated' => true),
+        array('name' => 'civicrm','href' => '/sites/default/ext/osdi/api/v3/{rel}','templated' => true),
        ),
     'self' => array('href' => '/sites/all/modules/civicrm/extern/osdi.php','title' => 'This API entry point'),
-    'osdi:people' => array('href' => '/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={%22sequential%22:1,%22return%22:%22magicword%22,%22magicword%22:%22sesame%22}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ',
+    'canvasser:brand_logo'=> array('href' => '/sites/default/files/logo.png',
+                            'title' => 'The branding logo for use in UI'),
+    'osdi:people' => array('href' => '/sites/default/ext/osdi/api/v3/People/index.php',
                             'title' => 'The collection of people in the system')
 );
 
