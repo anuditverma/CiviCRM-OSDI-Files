663d03794dda4392b6f431f0626751d429014b00
Anudit Verma <verma.anudit@gmail.com>
2015-08-09 11:05:08 +0530

    Updated osdi.php
    Added header

--- sites/all/modules/civicrm/extern/osdi.php
+++ sites/all/modules/civicrm/extern/osdi.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Content-Type:application/hal+json");
+
 $params['motd'] = 'Welcome to CiviCRM AEP!';
 $params['_links'] = array(
     'curies' => array(
@@ -10,7 +12,9 @@ $params['_links'] = array(
     'canvasser:brand_logo'=> array('href' => '/sites/default/files/logo.png',
                             'title' => 'The branding logo for use in UI'),
     'osdi:people' => array('href' => '/sites/default/ext/osdi/api/v3/People/index.php',
-                            'title' => 'The collection of people in the system')
+                            'title' => 'The collection of people in the system'),
+    'osdi:person_signup_helper' => array('href' => '/sites/default/ext/osdi/api/v3/post.php',
+    								'title' => 'Proxy signup helper')
 );
 
 echo json_encode($params);
